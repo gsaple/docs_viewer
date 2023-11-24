368ab649503ab5624118a8564c5c4561a242bf10
gsaple <yymelau@gmail.com>
2023-11-24 14:49:41 +1100

    front end bootstrap init
    front end bootstrap init

---
+++ front-end/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container py-4 px-3 mx-auto">
+        <h1>Hello, Bootstrap and Vite!</h1>
+        <div className="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
